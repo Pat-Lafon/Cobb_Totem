a68533b14419b27c861df190eb75e1fe3d75cf5b
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2026-02-12 13:45:08 -0500

    Add len to sort example

--- examples/list_sorted.ml
+++ examples/list_sorted.ml
@@ -3,6 +3,11 @@
 
 type [@grind] ilist = Nil | Cons of { head : int; tail : ilist }
 
+let [@simp] [@grind] rec len (l : ilist) : int =
+  match l with
+  | Nil -> 0
+  | Cons { head = _; tail = xs } -> 1 + len xs
+
 let [@simp] [@grind] rec sorted (l : ilist) : bool = match l with
 | Nil -> true
 | Cons { head = x; tail = xs } -> match xs with
